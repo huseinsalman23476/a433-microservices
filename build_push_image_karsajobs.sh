c3a980e6fec24f0a6e8092594b6cf15abc6993f9
huseinsalman23476 <huseinsalman23476@gmail.com>
2023-06-18 20:23:52 +0700

    Menambahkan Script Shell bernama build_push_image_karsajobs.sh untuk Build dan Push Docker Image ke GitHub Packages

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Membuat Docker image dari Dockerfile
+echo "------------------------------------------------------------------"
+echo "Membuat Docker image dari Dockerfile"
+echo "------------------------------------------------------------------"
+docker build -t karsajobs:latest .
+echo
+
+# Melihat daftar image di lokal
+echo "------------------------------------------------------------------"
+echo "Melihat daftar image di lokal"
+echo "------------------------------------------------------------------"
+docker images
+echo
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages
+echo "------------------------------------------------------------------"
+echo "Mengubah nama image agar sesuai dengan format GitHub Packages"
+echo "------------------------------------------------------------------"
+docker tag karsajobs:latest ghcr.io/huseinsalman23476/karsajobs:latest
+echo
+
+# Login ke GitHub Packages via Terminal
+echo "------------------------------------------------------------------"
+echo "Login ke GitHub Packages via Terminal"
+echo "------------------------------------------------------------------"
+echo $CR_PAT | docker login ghcr.io -u huseinsalman23476 --password-stdin
+echo
+
+# Mengunggah image ke GitHub Packages
+echo "------------------------------------------------------------------"
+echo "Mengunggah image ke GitHub Packages"
+echo "------------------------------------------------------------------"
+docker push ghcr.io/huseinsalman23476/karsajobs:latest
